dad7eefd221cf9316c08190593d6847c27f0f97c
Luis Douglas <douglasrodrigues37@gmail.com>
2022-03-11 22:52:36 -0300

    add tests_unit jest

---
+++ src/__tests__/index.unit.ts
@@ -0,0 +1,12 @@
+import { form, SHOW_ERROR_MESSAGES, shoulSendForm } from "../index";
+
+it("", () => {
+  console.log(SHOW_ERROR_MESSAGES);
+});
+it("", () => {
+  expect(shoulSendForm(form)).toBe(true);
+});
+
+it("", () => {
+  console.log(form);
+});
